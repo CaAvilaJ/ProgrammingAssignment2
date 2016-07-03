cd0ac0c527a6ec6ad6443967a7507a024959a35e
CaAvilaJ <CaAvilaJ@users.noreply.github.com>
2016-07-03 02:03:55 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## assigns inverse matrix as NON-SQUARE matrix
+## Returns a list of 4 functions
+##     1st Fn will set the value of the matrix
+##     2nd Fn will get the value of the matrix
+##     3rd Fn will set the value of the inverse
+##     4th Fn will get the value of the inverse
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	invrs <- matrix(nrow=1,ncol=2)		## create NON-SQUARE matrix
+	set <- function(y){
+		x <<- y
+		invrs <<- matrix(nrow=1,ncol=2)  ## create NON-SQUARE matrix
+	}
+	get <- function() x
+	setinverse <- function(solve) invrs <<- solve
+	getinverse <- function() invrs
+	list(set=set, get=get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes a list created from makeCacheMatrix as input
+## if cached inverse matrix is a SQUARE Matrix returns the inverse matrix
+## if cached inverse matrix is NON-SQUARE then calculates and returns inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	invrs <- x$getinverse()
+	if( nrow(invrs) == ncol(invrs) ) {
+		message("getting cached data")
+		return(invrs)
+	}
+	data <- x$get()
+	inv <- solve(data)
+	x$setinverse(inv)
+	inv
 }
